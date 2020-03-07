d36f05e4d5266e6283b3a112c4a47ddd8e32c0b7
Conor O Reilly <conororeilly@Conors-MacBook-Pro.local>
2020-03-07 16:44:01 +0000

    first testing

--- automation.py
+++ automation.py
@@ -2,4 +2,10 @@ from selenium import webdriver
 
 chrome_browser = webdriver.Chrome('./chromedriver')
 
-chrome_browser.maximise_window
+chrome_browser.maximize_window()
+chrome_browser.get('https://www.seleniumeasy.com/test/basic-first-form-demo.html')
+
+# print('Selenium Easy Demo' in chrome_browser.title) # Is word in app
+assert 'Selenium Easy Demo' in chrome_browser.title # Will exit if untrue
+button_text = chrome_browser.find_element_by_class_name('btn-default')
+print(button_text.get_attribute('innerHTML'))
