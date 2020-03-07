77dcfe300c2d3fb62e2c403f762de961b1029212
Conor O Reilly <conororeilly@Conors-MacBook-Pro.local>
2020-03-07 17:03:10 +0000

    basics of Selenium

--- automation.py
+++ automation.py
@@ -6,6 +6,19 @@ chrome_browser.maximize_window()
 chrome_browser.get('https://www.seleniumeasy.com/test/basic-first-form-demo.html')
 
 # print('Selenium Easy Demo' in chrome_browser.title) # Is word in app
-assert 'Selenium Easy Demo' in chrome_browser.title # Will exit if untrue
-button_text = chrome_browser.find_element_by_class_name('btn-default')
-print(button_text.get_attribute('innerHTML'))
+assert 'Selenium Easy Demo' in chrome_browser.title 
+show_message_button = chrome_browser.find_element_by_class_name('btn-default')
+print(show_message_button.get_attribute('innerHTML'))
+
+assert 'Show Message' in chrome_browser.page_source
+
+user_message = chrome_browser.find_element_by_id('user-message')
+user_button2 = chrome_browser.find_element_by_css_selector('#get-input > .btn')
+print(user_button2)
+user_message.clear
+user_message.send_keys('Hello')
+
+show_message_button.click()
+output_message = chrome_browser.find_element_by_id('display')
+
+assert 'Hello' in output_message.text
